0e78141117cb7c5662e2499f1bf31167405255bc
omarsanchez <omar.sanchez@wizeline.com>
2018-01-19 22:03:15 -0600

    Omarss

---
+++ app/src/main/java/com/wizeline/brainstormingapp/VoteTinder/VoteActivity.java
@@ -0,0 +1,15 @@
+package com.wizeline.brainstormingapp.VoteTinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wizeline.brainstormingapp.R;
+
+public class VoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+}
